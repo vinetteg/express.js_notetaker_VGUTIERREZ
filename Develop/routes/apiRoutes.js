dfe0229b89bd204af16f6ab4b269ce8bcd1d1bb1
Vinette Gutierrez <vinette.gutierrez@gmail.com>
2021-06-28 19:05:59 -0700

    Updated routes to connect successfully

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,30 +1,21 @@
-const fs = require('fs');
-
-
-
-
+const fs = require("fs");
+const notes = require("../db/db.json");
 module.exports = (app) => {
+  app.get("/api/notes", (req, res) => res.json(notes));
 
-  
-    app.get('/api/notes', (req, res) => res.json(notes);
-  
-  
-  
-    app.post('/api/notes', (req, res) => {
-      if (notes.length < 5) {
-        notes.push(req.body);
-        res.json(true);
-      } else {
-        res.json(false);
-      }
-    });
-  
-  
-    app.post('/api/notes', (req, res) => {
-      // Empty out the arrays of data
+  app.post("/api/notes", (req, res) => {
+    if (notes.length < 5) {
+      notes.push(req.body);
+      res.json(true);
+    } else {
+      res.json(false);
+    }
+  });
+
+  app.post("/api/notes", (req, res) => {
+    // Empty out the arrays of data
     notes.length = 0;
-  
-      res.json({ ok: true });
-    });
-  };
-  
+
+    res.json({ ok: true });
+  });
+};
--- Develop/server.js
+++ Develop/server.js
@@ -6,9 +6,13 @@ const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+// app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
-app.get("/add", (req, res) => res.sendFile(path.join(__dirname, "notes.html")));
+// app.get("/add", (req, res) => res.sendFile(path.join(__dirname, "notes.html")));
