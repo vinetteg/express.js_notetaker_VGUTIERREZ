968af6ccad83437db136ad30c10191dd12f60157
Vinette Gutierrez <vinette.gutierrez@gmail.com>
2021-06-28 22:52:58 -0700

    Updating routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
-const notes = require("../db/db.json");
+const notes = require("../../db/db.json");
 module.exports = (app) => {
   app.get("/api/notes", (req, res) => res.json(notes));
 
-  app.post("/api/notes", (req, res) => {
+  app.post("api/notes", (req, res) => {
     if (notes.length < 5) {
       notes.push(req.body);
       res.json(true);
@@ -12,7 +12,7 @@ module.exports = (app) => {
     }
   });
 
-  app.post("/api/notes", (req, res) => {
+  app.post("api/notes", (req, res) => {
     // Empty out the arrays of data
     notes.length = 0;
 
